82aad00bdc12d43b357b0ec788826857ddf5d608
Dylan Bragdon <dylan.bragdon@gmail.com>
2022-09-19 23:26:37 -0700

    moved arg handling to handleArgs and added checkKeywords and clearList function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -75,13 +76,57 @@ func popItem(db *sql.DB, itemId int) {
 }
 
 func createTable(db *sql.DB) {
+
 	_, err := db.Exec("CREATE TABLE IF NOT EXISTS todo (item text, time datetime);")
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Creating new todo instance.")
+}
+
+func clearList(db *sql.DB) {
+
+	var choice string
+
+	fmt.Println("Are you sure you want to clear your todo list? (y/n)")
+	for {
+
+		fmt.Scanln(&choice)
+
+		if choice == "y" {
+
+			_, err := db.Exec("DELETE FROM todo;")
+
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println("\nYour list is cleared.")
+
+			break
+		} else if choice == "n" {
+			return
+		} else {
+			fmt.Println("Invalid choice, please enter 'y' or 'n'")
+		}
+	}
+
+}
+
+func checkKeywords(args []string) error {
+
+	set := make(map[string]bool)
+
+	set["pop"] = true
+
+	if (len(args) == 1) && (set[args[0]]) {
+
+		return errors.New("Invalid command")
+
+	}
+
+	return nil
 }
 
 func initDB() *sql.DB {
@@ -93,22 +138,12 @@ func initDB() *sql.DB {
 		panic(err)
 	}
 
+	createTable(db)
+
 	return db
 }
 
-func main() {
-
-	db := initDB()
-
-	//db, err := sql.Open("sqlite3", "./todo.db")
-
-	//db.SetMaxOpenConns(1)
-
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	args := os.Args[1:]
+func handleArgs(db *sql.DB, args []string) {
 
 	if len(args) == 0 {
 
@@ -122,13 +157,19 @@ func main() {
 
 	} else if len(args) == 1 {
 
-		newItem := args[0]
+		if args[0] == "clear" {
+			clearList(db)
 
-		flag.Parse()
+		} else {
 
-		fmt.Println("Added new todo item: ", newItem)
+			newItem := args[0]
 
-		addItem(db, newItem)
+			flag.Parse()
+
+			fmt.Println("Added new todo item: ", newItem)
+
+			addItem(db, newItem)
+		}
 
 	} else if len(args) == 2 {
 		if args[0] == "pop" {
@@ -138,7 +179,24 @@ func main() {
 			}
 
 			popItem(db, popIndex)
+
 		}
 	}
+}
+
+func main() {
+
+	db := initDB()
+
+	args := os.Args[1:]
+
+	err := checkKeywords(args)
+
+	if err != nil {
+		fmt.Println("Invalid command")
+		os.Exit(1)
+	}
+
+	handleArgs(db, args)
 
 }
